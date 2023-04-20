5c444b601fca9c3e76d3b9b69534b964709ebef2
Joshua Slui <joshuaslui0203@gmail.com>
2023-04-20 10:04:30 +0200

    add extra unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -8,6 +8,16 @@ else:
 
 # schrijf zelf nog wat extra testen voor getNumberOfCharacters
 
+if getNumberOfCharacters('aansprakelijkheidswaardevaststellingsveranderingen') == 50:
+    print("Test geslaagd")
+else:
+    print("Deze test is niet geslaagd")
+
+if getNumberOfCharacters('Dit is een test zin') == 15:
+    print("Test geslaagd")
+else:
+    print("Deze test is niet geslaagd")
+
 # test 2: getNumberOfSentences
 if getNumberOfSentences(getText('easy')) == 14:
     print("Test geslaagd")
@@ -15,6 +25,11 @@ else:
     print("Deze test is niet geslaagd")
 
 # schrijf zelf nog een extra testen voor getNumberOfSentences (gebruik test.txt).
+
+if getNumberOfSentences(getText('data/test.txt')) == 4:
+    print("Test geslaagd")
+else:
+    print("Deze test is niet geslaagd")
 
 # test 3: getNumberOfWords
 print(getNumberOfWords(getText('data\difficult1.txt')))
@@ -29,3 +44,8 @@ else:
     print("Deze test is niet geslaagd")
 
 # schrijf zelf nog een extra testen voor getNumberOfWords (gebruik test.txt).
+
+if getNumberOfWords(getText('data/test.txt')) == 70:
+    print("Test geslaagd")
+else:
+    print("Deze test is niet geslaagd")
